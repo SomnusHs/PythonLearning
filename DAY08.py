7b004782cb1369dd851f4a53ccb6d1b4a53f4eb2
Renng Hua <2498224075@qq.com>
2026-04-20 10:39:07 +0800

    PythonLearningRecord
    DAY08

---
+++ DAY08.py
@@ -0,0 +1,147 @@
+### 前言
+
+# Example 1：将一颗色子掷6000次，统计每种点数出现的次数
+
+import random
+
+f1 = 0
+f2 = 0
+f3 = 0
+f4 = 0
+f5 = 0
+f6 = 0
+for _ in range(6000):
+    face = random.randrange(1, 7)
+    if face == 1:
+        f1 += 1
+    elif face == 2:
+        f2 += 1
+    elif face == 3:
+        f3 += 1
+    elif face == 4:
+        f4 += 1
+    elif face == 5:
+        f5 += 1
+    else:
+        f6 += 1
+print(f'1点出现了{f1}次')
+print(f'2点出现了{f2}次')
+print(f'3点出现了{f3}次')
+print(f'4点出现了{f4}次')
+print(f'5点出现了{f5}次')
+print(f'6点出现了{f6}次')
+
+
+
+### 创建列表
+
+# Example 1
+
+items1 = [35, 12, 99, 68, 55, 35, 87]
+items2 = ['Python', 'Java', 'Go', 'Kotlin']
+items3 = [100, 12.3, 'Python', True]
+print(items1)  # [35, 12, 99, 68, 55, 35, 87]
+print(items2)  # ['Python', 'Java', 'Go', 'Kotlin']
+print(items3)  # [100, 12.3, 'Python', True]
+# 使用type函数来查看变量的类型；通过 Python 内置的list函数将其他序列变成列表
+items4 = list(range(1, 10))
+items5 = list('hello')
+print(items4)  # [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(items5)  # ['h', 'e', 'l', 'l', 'o']
+
+
+
+### 列表的运算
+
+# Example 1
+
+# 使用+运算符实现两个列表的拼接
+items5 = [35, 12, 99, 45, 66]
+items6 = [45, 58, 29]
+items7 = ['Python', 'Java', 'JavaScript']
+print(items5 + items6)  # [35, 12, 99, 45, 66, 45, 58, 29]
+print(items6 + items7)  # [45, 58, 29, 'Python', 'Java', 'JavaScript']
+items5 += items6
+print(items5)  # [35, 12, 99, 45, 66, 45, 58, 29]
+# 使用*运算符实现列表的重复运算
+print(items6 * 3)  # [45, 58, 29, 45, 58, 29, 45, 58, 29]
+print(items7 * 2)  # ['Python', 'Java', 'JavaScript', 'Python', 'Java', 'JavaScript']
+# 使用in或not in运算符判断一个元素在不在列表中
+print(29 in items6)  # True
+print(99 in items6)  # False
+print('C++' not in items7)     # True
+print('Python' not in items7)  # False
+# 索引运算
+items8 = ['apple', 'waxberry', 'pitaya', 'peach', 'watermelon']
+print(items8[0])   # apple
+print(items8[2])   # pitaya
+print(items8[4])   # watermelon
+items8[2] = 'durian'
+print(items8)      # ['apple', 'waxberry', 'durian', 'peach', 'watermelon']
+print(items8[-5])  # 'apple'
+print(items8[-4])  # 'waxberry'
+print(items8[-1])  # watermelon
+items8[-4] = 'strawberry'
+print(items8)      # ['apple', 'strawberry', 'durian', 'peach', 'watermelon']
+# 切片运算
+print(items8[1:3:1])     # ['strawberry', 'durian']
+print(items8[0:3:1])     # ['apple', 'strawberry', 'durian']
+print(items8[0:5:2])     # ['apple', 'durian', 'watermelon']
+print(items8[-4:-2:1])   # ['strawberry', 'durian']
+print(items8[-2:-6:-1])  # ['peach', 'durian', 'strawberry', 'apple']
+# 切片运算省略版本
+print(items8[1:3])     # ['strawberry', 'durian']
+print(items8[:3:1])    # ['apple', 'strawberry', 'durian']
+print(items8[::2])     # ['apple', 'durian', 'watermelon']
+print(items8[-4:-2])   # ['strawberry', 'durian']
+print(items8[-2::-1])  # ['peach', 'durian', 'strawberry', 'apple']
+# 通过切片操作修改列表中的元素
+items8[1:3] = ['x', 'o']
+print(items8)  # ['apple', 'x', 'o', 'peach', 'watermelon']
+# 两个列表做关系运算
+nums1 = [1, 2, 3, 4]
+nums2 = list(range(1, 5))
+nums3 = [3, 2, 1]
+print(nums1 == nums2)  # True
+print(nums1 != nums2)  # False
+print(nums1 <= nums3)  # True
+print(nums2 >= nums3)  # False
+
+
+
+### 元素的遍历
+
+# Example 1：在循环结构中通过索引运算，遍历列表元素。
+
+languages = ['Python', 'Java', 'C++', 'Kotlin']
+for index in range(len(languages)):
+    print(languages[index])
+"""
+Python
+Java
+C++
+Kotlin
+"""
+
+# Example 2：直接对列表做循环，循环变量就是列表元素的代表。
+
+languages = ['Python', 'Java', 'C++', 'Kotlin']
+for language in languages:
+    print(language)
+
+
+
+### 总结
+
+# Example 1：回看开头例子。
+
+import random
+
+counters = [0] * 6
+# 模拟掷色子记录每种点数出现的次数
+for _ in range(6000):
+    face = random.randrange(1, 7)
+    counters[face - 1] += 1
+# 输出每种点数出现的次数
+for face in range(1, 7):
+    print(f'{face}点出现了{counters[face - 1]}次')
